2df4422a12d61f5c6115db884c5be01d7fc81d4e
Ali Kelkawi <ali.kelkawi@canonical.com>
2024-11-24 19:48:48 +0300

    add postgres db integration test

---
+++ resource_sample_py/resource_sample/activities/db_activity.py
@@ -0,0 +1,57 @@
+# Copyright 2023 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+from temporalio import activity
+import os
+import psycopg2
+from psycopg2 import sql
+from common.messages import ComposeGreetingInput
+
+
+@activity.defn(name="database_test")
+async def database_test(arg: ComposeGreetingInput) -> str:
+    db_config = {
+        "dbname": os.getenv("TEMPORAL_DB_NAME"),
+        "user": os.getenv("TEMPORAL_DB_USER"),
+        "password": os.getenv("TEMPORAL_DB_PASSWORD"),
+        "host": os.getenv("TEMPORAL_DB_HOST"),
+        "port": os.getenv("TEMPORAL_DB_PORT")
+    }
+    conn = psycopg2.connect(**db_config)
+    cursor = conn.cursor()
+    
+    # Create test table
+    table_name = "test_table"
+    create_table_query = sql.SQL("""
+        CREATE TABLE IF NOT EXISTS {table} (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(100) NOT NULL,
+            value INTEGER NOT NULL
+        )
+    """).format(table=sql.Identifier(table_name))
+    
+    cursor.execute(create_table_query)
+    conn.commit()
+    
+    # Insert sample record
+    insert_query = sql.SQL("INSERT INTO {table} (name, value) VALUES (%s, %s) RETURNING id").format(
+        table=sql.Identifier(table_name)
+    )
+    cursor.execute(insert_query, ("example_name", 123))
+    inserted_id = cursor.fetchone()[0]
+    conn.commit()
+    
+    # Read the record back
+    select_query = sql.SQL("SELECT * FROM {table} WHERE id = %s").format(
+        table=sql.Identifier(table_name)
+    )
+    cursor.execute(select_query, (inserted_id,))
+    record = cursor.fetchone()
+
+    # Close the cursor and connection
+    if cursor:
+        cursor.close()
+    if conn:
+        conn.close()
+
+    return record
---
+++ tests/integration/test_postgres_db.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# Copyright 2023 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+"""Temporal worker charm postgresql relation integration tests."""
+
+import logging
+
+import pytest
+from conftest import deploy  # noqa: F401, pylint: disable=W0611
+from helpers import APP_NAME, run_sample_workflow, scale
+from pytest_operator.plugin import OpsTest
+
+logger = logging.getLogger(__name__)
+
+
+@pytest.mark.abort_on_fail
+@pytest.mark.usefixtures("deploy")
+class TestDeployment:
+    """Integration tests for Temporal charm."""
+
+    async def test_postgres_relation(self, ops_test: OpsTest):
+        """Test Postgresql relation."""
+        await scale(ops_test, app=APP_NAME, units=2)
+
+        await ops_test.model.deploy("postgresql-k8s", channel="14/stable", trust=True)
+
+        async with ops_test.fast_forward():
+            await ops_test.model.wait_for_idle(
+                apps=["postgresql-k8s"],
+                status="active",
+                raise_on_blocked=False,
+                timeout=600,
+            )
+
+            logger.info("relating temporal-worker-k8s to postgresql-k8s charms")
+            await ops_test.model.integrate(APP_NAME, "postgresql-k8s")
+
+            await ops_test.model.wait_for_idle(
+                apps=[APP_NAME, "postgresql-k8s"],
+                status="active",
+                raise_on_blocked=False,
+                timeout=600,
+            )
+
+            await run_sample_workflow(ops_test, workflow_type="database")
