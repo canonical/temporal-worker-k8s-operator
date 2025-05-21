82d0e91d6872d6c5dfe99c1d038532ed3a469738
yakupt-canonical <yakup.turgut@canonical.com>
2025-05-21 09:25:21 +0300

    add support for workload metrics

--- src/charm.py
+++ src/charm.py
@@ -81,6 +81,15 @@ class TemporalWorkerK8SOperatorCharm(CharmBase):
             refresh_event=self.on.config_changed,
         )
 
+        if self.config.get("workload-prometheus-port"):
+            WORKLOAD_PROMETHEUS_PORT = self.config.get("workload-prometheus-port")
+            self._app_prometheus_scraping = MetricsEndpointProvider(
+                self,
+                relation_name="workload-metrics-endpoint",
+                jobs=[{"static_configs": [{"targets": [f"*:{WORKLOAD_PROMETHEUS_PORT}"]}]}],
+                refresh_event=self.on.config_changed,
+            )
+
         # Loki
         self._log_forwarder = LogForwarder(self, relation_name="logging")
 
