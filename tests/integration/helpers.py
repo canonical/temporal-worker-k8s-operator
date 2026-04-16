c73cdff49b590c736613cc133d56ea0879bf5de8
nikita-jare <nikitajare2021@gmail.com>
2026-04-16 11:47:20 -0400

    change ubunutu base temporal_k8s

--- tests/integration/helpers.py
+++ tests/integration/helpers.py
@@ -271,7 +271,9 @@ async def setup_temporal_ecosystem(ops_test: OpsTest):
         ops_test: PyTest object.
     """
     await asyncio.gather(
-        ops_test.model.deploy(APP_NAME_SERVER, channel="edge", config={"num-history-shards": 1}),
+        ops_test.model.deploy(
+            APP_NAME_SERVER, channel="1.23/edge", config={"num-history-shards": 1}, base="ubuntu@24.04"
+        ),
         ops_test.model.deploy(APP_NAME_ADMIN, channel="edge"),
         ops_test.model.deploy("postgresql-k8s", channel="14/stable", trust=True, revision=381),
     )
